fb029fe996bfe21bf45d66f24c85af18d8e9fb6d
wandera7 <julius.wandera@student.moringaschool.com>
2022-11-26 23:44:09 +0300

    New Navbar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 function Navbar(){
     return(
-        <div className="bg-blue-800 p-5 flex justify-end">
+        <div className="bg-primary-200 p-5 flex justify-end">
 
             <NavLink className="p-3 text-white ml-32 " exact to='/'>HOME</NavLink>
             <NavLink exact className="p-3   " to='/login'>LOGIN</NavLink>
