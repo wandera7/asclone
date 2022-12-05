4a2401d3856e56a0c010f8b6c15d9a2a47b57f28
wandera7 <julius.wandera@student.moringaschool.com>
2022-12-05 20:08:10 +0300

    Link added to Foreman component

--- src/components/Foreman.js
+++ src/components/Foreman.js
@@ -1,14 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Foreman({man}){
-    const {name, img, summary , age , location} = man;
+    const {id,name, img, summary , age , location} = man;
     return(
         <div>
         <h2>{name}</h2>
         <h3>{location}</h3>
         <h4>{age}</h4>
         <img src={img} alt="Foreman pic"/>
-        <p>{summary}</p>
+        <p>
+        {summary}
+        <h5>...<Link to={`/foreman/${id}`}>ReadMore</Link></h5>
+        </p>
         </div>
     )
 }
