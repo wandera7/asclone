203049c75334fd622c771c871bb2ec611ada0d4a
wandera7 <julius.wandera@student.moringaschool.com>
2022-12-05 19:55:53 +0300

    Get foremen data

--- src/components/Userpage.js
+++ src/components/Userpage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link,Redirect } from "react-router-dom";
 function Userpage({user}){
+    const [foremen, setForemen] = useState([]);
+    useEffect(()=>{
+        fetch('')
+        .then((r)=>{
+            if(r.ok){
+                r.json().then((data)=>setForemen(data));
+            }else{
+                r.json().then((err)=>{
+                    console.log(err);
+                });
+
+            }
+        })
+    },[])
+
     if (!user) return <Redirect to="/loginpage" />
     const id=2
     return(
         <div className="text-gray-600">
             Welcome new user
+
             <h5>...<Link to={`/foreman/${id}`}>ReadMore</Link></h5>
         </div>
     )
