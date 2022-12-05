d686337741ebb6f26fa4208a8d0d312ba1da9804
wandera7 <julius.wandera@student.moringaschool.com>
2022-12-05 20:14:30 +0300

    Fetched a particular foreman

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {Redirect, useParams} from "react-router-dom";
 function Details({user}){
     const params = useParams();
     const [person,setPerson] = useState({})
+    useEffect(()=>{
+        fetch('')
+        .then((r)=>{
+            if(r.ok){
+                r.json().then((foreman)=>setPerson(foreman))
+            }else{
+                r.json().then((err)=>console.log(err))
+            }
+        })
+    },[params.id])
     if (!user) return <Redirect to="/loginpage" />
     return(
         <div>
