3f8850031b620fddd82831e3d400f55ed49385b9
wandera7 <julius.wandera@student.moringaschool.com>
2022-12-05 20:05:42 +0300

    Foreman div

--- src/components/Foreman.js
+++ src/components/Foreman.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 function Foreman({man}){
-    const {name, img , summary , age , location} = man;
+    const {name, img, summary , age , location} = man;
     return(
         <div>
-        <h2>name</h2>
+        <h2>{name}</h2>
+        <h3>{location}</h3>
+        <h4>{age}</h4>
+        <img src={img} alt="Foreman pic"/>
+        <p>{summary}</p>
         </div>
     )
 }
